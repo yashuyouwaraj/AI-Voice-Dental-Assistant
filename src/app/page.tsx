42b4d50b10afbe18ec5deb7760857c6ce0c66584
Yashu Youwaraj <amanyasu@gmail.com>
2026-03-04 11:44:54 +0530

    added user sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,15 @@ import PricingSection from "@/components/landing/PricingSection";
 import WhatToAsk from "@/components/landing/WhatToAsk";
 import CTA from "@/components/landing/CTA";
 import Footer from "@/components/landing/Footer";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser()
+
+  //redirect auth user to dashboard
+  if(user) redirect("/dashboard")
   return (
     <div className="min-h-screen bg-background">
       <Header />
